65abd07cdf08f10046f5bb74f669a0ed5dd382c4
Stuart <mcdonnell.stuart+github@gmail.com>
2023-08-27 11:49:45 +1000

    Changed code to get API key from environment variables

--- src/services/superbase.js
+++ src/services/superbase.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-//import { env } from 'node:process';
-const supabaseUrl = "https://aizlrqfnpxtfciqxiphw.supabase.co";
-const supabaseKey =
- "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpemxycWZucHh0ZmNpcXhpcGh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4MzgwNjUsImV4cCI6MjAwNjQxNDA2NX0.9nDDB4VQQwNTdzAMoV1hlyMNq53idKs7LpN7JV2pbU0";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 //const supabase = createClient(supabaseUrl, env.SUPABASE_KEY);
 
